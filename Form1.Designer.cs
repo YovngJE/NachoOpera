1b1faafe96f3ec03670a82dc75c4b45e6b55ab80
YovngJE <jhonatan.perea579@pascualbravo.edu.co>
2025-03-07 15:26:47 -0500

    Segunda Version: integracion de las librerias Estilo a la ventana Codigo de los botones

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NachoOpera
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -37,90 +37,147 @@
             this.btnMultiplicar = new System.Windows.Forms.Button();
             this.btnDividir = new System.Windows.Forms.Button();
             this.lblResultado = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblNum1
             // 
             this.lblNum1.AutoSize = true;
-            this.lblNum1.Location = new System.Drawing.Point(67, 105);
+            this.lblNum1.Location = new System.Drawing.Point(117, 164);
+            this.lblNum1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblNum1.Name = "lblNum1";
-            this.lblNum1.Size = new System.Drawing.Size(68, 16);
+            this.lblNum1.Size = new System.Drawing.Size(130, 25);
             this.lblNum1.TabIndex = 0;
             this.lblNum1.Text = "Numero 1:";
             // 
             // lblNum2
             // 
             this.lblNum2.AutoSize = true;
-            this.lblNum2.Location = new System.Drawing.Point(67, 145);
+            this.lblNum2.Location = new System.Drawing.Point(117, 227);
+            this.lblNum2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblNum2.Name = "lblNum2";
-            this.lblNum2.Size = new System.Drawing.Size(68, 16);
+            this.lblNum2.Size = new System.Drawing.Size(130, 25);
             this.lblNum2.TabIndex = 1;
             this.lblNum2.Text = "Numero 2:";
             // 
             // txtNum1
             // 
-            this.txtNum1.Location = new System.Drawing.Point(150, 105);
+            this.txtNum1.BackColor = System.Drawing.Color.DarkGreen;
+            this.txtNum1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNum1.Location = new System.Drawing.Point(262, 164);
+            this.txtNum1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtNum1.Name = "txtNum1";
-            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.Size = new System.Drawing.Size(172, 32);
             this.txtNum1.TabIndex = 2;
             // 
             // txtNum2
             // 
-            this.txtNum2.Location = new System.Drawing.Point(150, 145);
+            this.txtNum2.BackColor = System.Drawing.Color.DarkGreen;
+            this.txtNum2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNum2.Location = new System.Drawing.Point(262, 227);
+            this.txtNum2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtNum2.Name = "txtNum2";
-            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.Size = new System.Drawing.Size(172, 32);
             this.txtNum2.TabIndex = 3;
             // 
             // btnSumar
             // 
-            this.btnSumar.Location = new System.Drawing.Point(86, 289);
+            this.btnSumar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnSumar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSumar.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.Location = new System.Drawing.Point(122, 452);
+            this.btnSumar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnSumar.Name = "btnSumar";
-            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.Size = new System.Drawing.Size(164, 61);
             this.btnSumar.TabIndex = 4;
             this.btnSumar.Text = "Sumar";
-            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.UseVisualStyleBackColor = false;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
             // 
             // btnRestar
             // 
-            this.btnRestar.Location = new System.Drawing.Point(186, 289);
+            this.btnRestar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnRestar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRestar.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestar.Location = new System.Drawing.Point(326, 452);
+            this.btnRestar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnRestar.Name = "btnRestar";
-            this.btnRestar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestar.Size = new System.Drawing.Size(149, 61);
             this.btnRestar.TabIndex = 5;
             this.btnRestar.Text = "Restar";
-            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.UseVisualStyleBackColor = false;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
             // 
             // btnMultiplicar
             // 
-            this.btnMultiplicar.Location = new System.Drawing.Point(292, 289);
+            this.btnMultiplicar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(511, 452);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnMultiplicar.Name = "btnMultiplicar";
-            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.Size = new System.Drawing.Size(212, 61);
             this.btnMultiplicar.TabIndex = 6;
             this.btnMultiplicar.Text = "Multiplicar";
-            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
             // 
             // btnDividir
             // 
-            this.btnDividir.Location = new System.Drawing.Point(404, 289);
+            this.btnDividir.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDividir.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(749, 452);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnDividir.Name = "btnDividir";
-            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.Size = new System.Drawing.Size(187, 61);
             this.btnDividir.TabIndex = 7;
             this.btnDividir.Text = "Dividir";
-            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
             // 
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(543, 111);
+            this.lblResultado.Location = new System.Drawing.Point(950, 173);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(69, 16);
+            this.lblResultado.Size = new System.Drawing.Size(0, 25);
             this.lblResultado.TabIndex = 8;
-            this.lblResultado.Text = "Resultado";
             // 
-            // Form1
+            // btnLimpiar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.btnLimpiar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(1138, 464);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(180, 48);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(950, 127);
+            this.lblMensaje.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(132, 25);
+            this.lblMensaje.TabIndex = 10;
+            this.lblMensaje.Text = "Resultado:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1400, 703);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.btnDividir);
             this.Controls.Add(this.btnMultiplicar);
@@ -130,8 +187,10 @@
             this.Controls.Add(this.txtNum1);
             this.Controls.Add(this.lblNum2);
             this.Controls.Add(this.lblNum1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmPrincipal";
+            this.Text = "Nacho Opera";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -149,6 +208,8 @@
         private System.Windows.Forms.Button btnMultiplicar;
         private System.Windows.Forms.Button btnDividir;
         private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblMensaje;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,12 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BibliotecaMultiplicacion;
+using BibliotecaResta;
+using BibliotecaSuma;
+using Division;
+using Libreria;
 
 namespace NachoOpera
 {
-    public partial class Form1 : Form
+   
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        long  resultado;
+        public frmPrincipal()
         {
             InitializeComponent();
         }
@@ -20,6 +27,47 @@ namespace NachoOpera
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+             
+             resultado =BibliotecaSuma.Class1.Sumar(int.Parse(txtNum1.Text),int.Parse(txtNum2.Text));
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            resultado=BibliotecaResta.Class1.Resta(int.Parse(txtNum1.Text), int.Parse(txtNum2.Text));
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            resultado = BibliotecaMultiplicacion.Class1.Multiplicacion(int.Parse(txtNum1.Text), int.Parse(txtNum2.Text));
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+           
+            if (int.Parse(txtNum2.Text) == 0)
+            {
+                MessageBox.Show("No se puede dividir por cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                resultado = Division.Class1.Dividir(int.Parse(txtNum1.Text), int.Parse(txtNum2.Text));
+                lblResultado.Text = resultado.ToString();
+            }
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            lblResultado.Text = "0";
         }
     }
 }
